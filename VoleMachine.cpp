5af05ba1dcc9f258dcff0d351e3a89263cd26564
qusayaqeel <qsyqyl1000@gmail.com>
2024-11-07 14:33:43 +0300

    Adding memory class implementation

--- VoleMachine.cpp
+++ VoleMachine.cpp
@@ -1,1 +1,35 @@
 #include "VoleMachine.h"
+using namespace std ;
+Memory::Memory(int size) : cells(size, "00") {}
+
+string Memory::getCell(size_t addr) const {
+    if (addr >= cells.size()) {
+        cerr << "Error: Memory address out of bounds!\n";
+        return "00";
+    }
+    return cells[addr];
+}
+
+void Memory::setCell(size_t addr, const string& value) {
+    if (addr >= cells.size()) {
+        cerr << "Error: Memory address out of bounds!\n";
+        return;
+    } else {
+        cells[addr] = value.size() < 2 ? ("0" + value) : value;
+    }
+}
+
+int Memory::getSize() const {
+    return cells.size();
+}
+
+void Memory::display() const {
+    cout << "Memory Contents:\n";
+    for (size_t i = 0; i < cells.size(); i += 16) {
+        cout << hex << uppercase << setw(1) << setfill('0') << (i / 16) << ": ";
+        for (size_t j = i; j < i + 16 && j < cells.size(); ++j) {
+            cout << cells[j] << " ";
+        }
+        cout <<endl;
+   }
+}
